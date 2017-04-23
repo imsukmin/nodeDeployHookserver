3fc8a1152b48bbd3ce40324134ebcc1c2890bfde
imsukmin <imsukmin@gmail.com>
2017-04-23 15:12:05 +0900

    arranged code

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
-var http    = require('http');
-var spawn   = require('child_process').spawn;
-var crypto  = require('crypto');
-var url     = require('url');
+var http = require('http')
+var spawn = require('child_process').spawn
+var crypto = require('crypto')
+var url = require('url')
 
-var secret  = 'nGenius'; // secret key of the webhook
-var port    = 40080; // port
+var secret = 'nGenius' // secret key of the webhook
+var port = 40080 // port
 
-http.createServer(function(req, res){
-    
-    console.log("request received");
-    res.writeHead(400, {"Content-Type": "application/json"});
+http.createServer(function (req, res) {
 
-    var path = url.parse(req.url).pathname;
+  console.log("request received")
+  res.writeHead(400, {
+    "Content-Type": "application/json"
+  })
 
-    if(path!='/push' || req.method != 'POST'){
-       var data = JSON.stringify({"error": "invalid request"});
-       return res.end(data); 
+  var path = url.parse(req.url).pathname
+
+  if (path != '/push' || req.method != 'POST') {
+    var data = JSON.stringify({
+      "error": "invalid request"
+    })
+    return res.end(data)
+  }
+
+  var jsonString = ''
+  req.on('data', function (data) {
+    jsonString += data
+  })
+
+  req.on('end', function () {
+    var hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex')
+    if (hash != req.headers['x-hub-signature']) {
+      console.log('invalid key')
+      var data = JSON.stringify({
+        "error": "invalid key",
+        key: hash
+      })
+      return res.end(data)
     }
 
+    console.log("running hook.sh")
 
-    var jsonString = '';
-    req.on('data', function(data){
-        jsonString += data;
-    });
+    var deploySh = spawn('sh', ['hook.sh'])
+    deploySh.stdout.on('data', function (data) {
+      var buff = new Buffer(data)
+      console.log(buff.toString('utf-8'))
+    })
 
-    req.on('end', function(){
-      var hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex');
-      if(hash != req.headers['x-hub-signature']){
-          console.log('invalid key');
-          var data = JSON.stringify({"error": "invalid key", key: hash});
-          return res.end(data);
-      } 
-       
-      console.log("running hook.sh");
-   
-      var deploySh = spawn('sh', ['hook.sh']);
-      deploySh.stdout.on('data', function(data){
-          var buff = new Buffer(data);
-          console.log(buff.toString('utf-8'));
-      });
+    res.writeHead(400, {
+      "Content-Type": "application/json"
+    })
 
-      
-    res.writeHead(400, {"Content-Type": "application/json"});
-    
-    var data = JSON.stringify({"success": true});
-      return res.end(data);
- 
-    });
+    var data = JSON.stringify({
+      "success": true
+    })
+    return res.end(data)
+  })
+}).listen(port)
 
-    
-}).listen(port);
-
-console.log("Server listening at " + port);
+console.log("Server listening at " + port)
